e5e077cf1907f27f9c987065e843f9eb64b4abc6
fakemailsam <120333682+fakemailsam@users.noreply.github.com>
2023-08-17 23:17:23 +0100

    Update server.py

--- server.py
+++ server.py
@@ -3,6 +3,7 @@ from main2 import app
 from main3 import test
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8181)
     test()
+    uvicorn.run(app, host="0.0.0.0", port=8181)
+    
 
